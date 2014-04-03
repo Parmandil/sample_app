b93c03e4a07c4597c61e7768a9fb48b381929b64
Melanie Hatch <parmandil13@gmail.com>
2014-04-02 17:30:33 -0700

    Created helper function for title generation, used it in layout.
    Functional difference is that if a page title is not defined,
    now the title will be the base title alone, no bar at the end.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #Returns the full title on a per-page basis.
+  #Returns the base title,
+  #and if there is a page title, adds a vertical bar and the page title.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
