758033050447358a4c848c7b9516c481b956d3cd
Melanie Hatch <parmandil13@gmail.com>
2014-05-19 10:30:02 -0700

    Finish user signup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
   before_save { email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -7,5 +6,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
   validates :password, length: { minimum: 6 }
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Saruman of Many Colors"
+    email    "curunir@orthanc.com"
+    password "joinw/thatPower"
+    password_confirmation "joinw/thatPower"
+  end
+end
